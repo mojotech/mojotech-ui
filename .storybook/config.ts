7efae75b78debf581b12ff9ffe04d0a84fa90ffb
Seth Krasnianski <sethkrasnianski@gmail.com>
2018-12-04 09:55:54 -0500

    Configure storybook to use TypeScript & .tsx files

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/react';
 
 // automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+const req = require.context('../stories', true, /.stories.tsx$/);
+
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+    req.keys().forEach(req);
 }
 
 configure(loadStories, module);
