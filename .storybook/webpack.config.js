a6d1714aebd958c17b83e02477e04df66b8eddfa
chriswynn <chris@wynncodes.com>
2019-03-14 15:44:17 -0400

    Update webpack config to match new storybook api

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,12 @@
-const path = require('path');
-const TSDocgenPlugin = require('react-docgen-typescript-webpack-plugin');
+const path = require("path");
+const TSDocgenPlugin = require("react-docgen-typescript-webpack-plugin");
 
-module.exports = (baseConfig, env, config) => {
+module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
-    loader: require.resolve('awesome-typescript-loader'),
+    loader: require.resolve("awesome-typescript-loader")
   });
   config.plugins.push(new TSDocgenPlugin()); // optional
-  config.resolve.extensions.push('.ts', '.tsx');
+  config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
