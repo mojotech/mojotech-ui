8148c7edf54639c3dcc55933b2258f4a21861883
chriswynn <chris@wynncodes.com>
2019-04-10 11:52:10 -0400

    Add maxWidths to theme

--- lib/theme.ts
+++ lib/theme.ts
@@ -6,6 +6,7 @@ export default {
     main: `'GT America Light', system-ui, sans-serif`,
     display: `'Altis Regular', system-ui, sans-serif`,
   },
+  maxWidths: ["904px", "1400px"],
   fontSizes: [
     "calc(16px + (22 - 16) * ((100vw - 400px) / (1800 - 600)))",
     "calc(18px + (28 - 18) * ((100vw - 400px) / (1800 - 600)))",
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,6 +2,7 @@ export type Theme = {
   color: {
     mojogreen: string;
   };
+  maxWidths: string[];
   fonts: { main: string; display: string };
   fontSizes: string[];
 };
