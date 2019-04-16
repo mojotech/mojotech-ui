1c31d3844be1a0f87d4936616fca3125f5f11de9
Jesse Hoyos <jesse@mojotech.com>
2019-04-16 15:43:51 -0400

    [refactor] remove references to document from font componenet

--- src/components/Font/index.tsx
+++ src/components/Font/index.tsx
@@ -1,26 +1,10 @@
 /** @jsx jsx */
-import * as React from "react";
 import { css, jsx, Global } from "@emotion/core";
 
 import { GT_AMERICA, MOJO_ALTIS } from "../../../lib/font-data";
 import t from "../../../lib/theme";
 
 export default () => {
-  React.useEffect(() => {
-    // Typescript is not acknowledging the fontface constructor for some reaason
-    // @ts-ignore
-    const gt_america = new FontFace("GT America Light", `url(${GT_AMERICA})`);
-    // @ts-ignore
-    const mojo_altis = new FontFace("altis-mojoregular", `url(${MOJO_ALTIS}})`);
-    Promise.all([mojo_altis.load(), gt_america.load()]).then(loadedFonts => {
-      loadedFonts.forEach(font => {
-        // also doesnt seem to know about `document.fonts`
-        // @ts-ignore
-        document.fonts.add(font);
-      });
-    });
-  });
-
   return (
     <Global
       styles={css`
