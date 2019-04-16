d46d3e9e13b367ebee0378c4fb5acdebc32e2839
chriswynn <chris@wynncodes.com>
2019-04-16 10:06:08 -0400

    Add Section component

---
+++ src/components/Section/index.tsx
@@ -0,0 +1,26 @@
+/** @jsx jsx */
+import * as React from "react";
+import { jsx } from "@emotion/core";
+import { Theme } from "../../types/global";
+
+type MarginBottom = 1 | 2 | 3 | 4 | 5 | 6;
+
+interface Props {
+  as?: string;
+  marginBottom?: MarginBottom;
+}
+
+const Section: React.FC<Props> = ({
+  as: S = "section",
+  marginBottom = 6,
+  ...props
+}) => (
+  <S
+    css={({ ...theme }: Theme) => ({
+      marginBottom: marginBottom && theme.spacing[marginBottom - 1],
+    })}
+    {...props}
+  />
+);
+
+export default Section;
---
+++ stories/section.stories.tsx
@@ -0,0 +1,114 @@
+import * as React from "react";
+import { storiesOf } from "@storybook/react";
+import Section from "../src/components/Section";
+import Wrap from "../src/components/Wrap";
+import Row from "../src/components/Row";
+import Column from "../src/components/Column";
+import Text from "../src/components/Text";
+
+storiesOf("Section", module).add("Sections", () => (
+  <React.Fragment>
+    <Section>
+      <Wrap size={2}>
+        <Text size={3} as="h2">
+          The principles that guide us.
+        </Text>
+        <Row>
+          <Column>
+            <Text as="h3" size={2}>
+              Agility
+            </Text>
+            <Text as="p">
+              We’ve successfully built complex systems a hundred times over. We
+              are able to identify issues quickly and adjust in real-time to
+              ensure we are tracking well against expectations and constantly
+              delivering value.
+            </Text>
+          </Column>
+          <Column>
+            <Text as="h3" size={2}>
+              Team Strength
+            </Text>
+            <Text as="p">
+              Designers and engineers are assigned to one, full-time project.
+              This allows the team to focus solely on clients’ goals,
+              internalizing a product vision, and taking pride and ownership in
+              their work.
+            </Text>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Text as="h3" size={2}>
+              User-Centered Design
+            </Text>
+            <Text as="p">
+              Our designs are built on a deep understanding of your business,
+              your customers, and what you want to achieve.
+            </Text>
+          </Column>
+          <Column>
+            <Text as="h3" size={2}>
+              User-Centered Design
+            </Text>
+            <Text as="p">
+              Our designs are built on a deep understanding of your business,
+              your customers, and what you want to achieve.
+            </Text>
+          </Column>
+        </Row>
+      </Wrap>
+    </Section>
+    <Section>
+      <Wrap size={2}>
+        <Text size={3} as="h2">
+          The principles that guide us.
+        </Text>
+        <Row>
+          <Column>
+            <Text as="h3" size={2}>
+              Agility
+            </Text>
+            <Text as="p">
+              We’ve successfully built complex systems a hundred times over. We
+              are able to identify issues quickly and adjust in real-time to
+              ensure we are tracking well against expectations and constantly
+              delivering value.
+            </Text>
+          </Column>
+          <Column>
+            <Text as="h3" size={2}>
+              Team Strength
+            </Text>
+            <Text as="p">
+              Designers and engineers are assigned to one, full-time project.
+              This allows the team to focus solely on clients’ goals,
+              internalizing a product vision, and taking pride and ownership in
+              their work.
+            </Text>
+          </Column>
+        </Row>
+        <Row>
+          <Column>
+            <Text as="h3" size={2}>
+              User-Centered Design
+            </Text>
+            <Text as="p">
+              Our designs are built on a deep understanding of your business,
+              your customers, and what you want to achieve.
+            </Text>
+          </Column>
+          <Column>
+            <Text as="h3" size={2}>
+              User-Centered Design
+            </Text>
+            <Text as="p">
+              Our designs are built on a deep understanding of your business,
+              your customers, and what you want to achieve.
+            </Text>
+          </Column>
+        </Row>
+      </Wrap>
+    </Section>
+  </React.Fragment>
+));
