9d1c7036c28f828d0b403279555a5727e1ba6810
chriswynn <chris@wynncodes.com>
2019-04-16 12:24:31 -0400

    Add BaseTheme export to index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { default as BaseTheme } from "../lib/theme";
 export { default as Button } from "./components/Button";
 export { default as Text } from "./components/Text";
 export { default as Reset } from "./components/Reset";
