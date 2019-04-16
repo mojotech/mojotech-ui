d7fd7324556f27d5309d337ac213d8203ceefeb4
Jesse Hoyos <jesse@mojotech.com>
2019-04-16 15:44:51 -0400

    [refactor] properly link theme type to styled function

--- lib/styled.tsx
+++ lib/styled.tsx
@@ -2,6 +2,6 @@
  * Exporting our own styled function wrapper allows us to have type save theme props
  */
 import styled, { CreateStyled } from "@emotion/styled";
-import { Theme } from "../src/types";
+import { Theme } from "../src/types/global";
 
 export default styled as CreateStyled<Theme>;
