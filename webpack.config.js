4a7f8aeff5883be1a18d50d4576c12fe34deb554
chriswynn <chris@wynncodes.com>
2019-04-22 11:52:25 -0400

    Add babel config to webpack build process

--- webpack.config.js
+++ webpack.config.js
@@ -7,16 +7,27 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "awesome-typescript-loader",
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+        use: {
+          loader: "awesome-typescript-loader",
+          options: {
+            useBabel: true,
+            babelOptions: {
+              babelrc: false,
+              presets: ["@babel/preset-env"],
+              plugins: ["emotion"],
+            },
+            babelCore: "@babel/core",
+          },
+        },
+      },
+    ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"]
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
 };
