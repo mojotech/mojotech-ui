6143bdb7995272ae8dafca4bfa6e8ce23af354bb
chriswynn <chris@wynncodes.com>
2019-04-29 11:10:32 -0400

    Update Wrap component styles

--- src/components/Wrap/index.tsx
+++ src/components/Wrap/index.tsx
@@ -1,9 +1,10 @@
 /** @jsx jsx */
 import * as React from "react";
-import { jsx, css } from "@emotion/core";
+import { jsx } from "@emotion/core";
+import { mq } from "../../../lib/utils";
 import { Theme } from "../../types/global";
 
-type MaxWidths = 1 | 2 | 3;
+type MaxWidths = 1 | 2;
 
 interface Props {
   as?: string;
@@ -11,15 +12,21 @@ interface Props {
 }
 
 const Wrap: React.FC<Props> = ({ as: W = "div", size = 1, ...props }) => {
-  const styles = ({ ...theme }: Theme) =>
-    css({
-      marginLeft: "auto",
-      marginRight: "auto",
-      maxWidth: size && theme.maxWidths[size - 1],
-      width: "100%",
-    });
-
-  return <W css={styles} {...props} />;
+  return (
+    <W
+      css={({ ...theme }: Theme) =>
+        mq({
+          paddingLeft: theme.spacing[3],
+          paddingRight: theme.spacing[3],
+          marginLeft: "auto",
+          marginRight: "auto",
+          maxWidth: [theme.maxWidths[0], size && theme.maxWidths[size - 1]],
+          width: "100%",
+        })
+      }
+      {...props}
+    />
+  );
 };
 
 export default Wrap;
