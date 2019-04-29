b5509bd3bbfcf2e28efcfbe61f7173f9f964b02e
chriswynn <chris@wynncodes.com>
2019-04-29 15:00:46 -0400

    Add 0 as a possible value for SpaceScale on Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -5,7 +5,7 @@ import { Theme } from "types/global";
 
 type TextTags = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "a";
 type FontSizes = 1 | 2 | 3 | 4 | 5 | 6;
-type SpaceScale = 1 | 2 | 3 | 4 | 5 | 6;
+type SpaceScale = 0 | 1 | 2 | 3 | 4 | 5 | 6;
 
 interface Props {
   as?: TextTags;
