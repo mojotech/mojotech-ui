09a943b567ac48834e3a57704196b7d4aa8fff06
chriswynn <chris@wynncodes.com>
2019-05-01 13:31:08 -0400

    Move lib folder into src

--- src/lib/styled.tsx
+++ src/lib/styled.tsx
@@ -2,6 +2,6 @@
  * Exporting our own styled function wrapper allows us to have type save theme props
  */
 import styled, { CreateStyled } from "@emotion/styled";
-import { Theme } from "../src/types/global";
+import { Theme } from "types/global";
 
 export default styled as CreateStyled<Theme>;
