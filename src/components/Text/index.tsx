5d6fea0601d6e3731d74b45ea0f08fcbc302e69b
chriswynn <chris@wynncodes.com>
2019-05-06 14:49:45 -0400

    Add anchor tag as possible tag on Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { jsx } from "@emotion/core";
 import { Theme } from "types/global";
 
-type TextTags = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span";
+type TextTags = "p" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "a";
 type FontSizes = 1 | 2 | 3 | 4 | 5 | 6;
 type SpaceScale = 1 | 2 | 3 | 4 | 5 | 6;
 
