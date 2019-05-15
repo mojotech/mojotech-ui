13475715c44cd94244de17be821958d6d3ffa649
chriswynn <chris@wynncodes.com>
2019-05-15 11:54:56 -0400

    Add marginBottom to Row component

--- src/components/Row/index.tsx
+++ src/components/Row/index.tsx
@@ -2,11 +2,18 @@
 import { jsx } from "@emotion/core";
 import { Theme } from "types/global";
 
-const Row: React.FC = props => (
+type MarginBottom = 1 | 2 | 3 | 4 | 5 | 6;
+
+interface Props {
+  marginBottom?: MarginBottom;
+}
+
+const Row: React.FC<Props> = ({ marginBottom = 4, ...props }) => (
   <div
     css={({ ...theme }: Theme) => ({
       display: "flex",
       flexWrap: "wrap",
+      marginBottom: marginBottom && theme.spacing[marginBottom - 1],
       marginLeft: `calc(${theme.spacing[2]} * -1)`,
       marginRight: `calc(${theme.spacing[2]} * -1)`,
       width: "100%",
