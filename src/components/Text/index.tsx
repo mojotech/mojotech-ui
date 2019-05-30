0f2760ae5ed4677392135c9e88bee1be69118c97
chriswynn <chris@wynncodes.com>
2019-05-30 13:25:00 -0400

    Remove anchor tag specific styles from Text component

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -25,7 +25,6 @@ const Text: React.FC<Props> = ({
 }) => (
   <T
     css={({ ...theme }: Theme) => ({
-      cursor: T === "a" ? "pointer" : "inherit",
       fontFamily: display ? theme.fonts.display : theme.fonts.main,
       fontSize: size && theme.fontSizes[size - 1],
       lineHeight:
@@ -35,11 +34,7 @@ const Text: React.FC<Props> = ({
           ? theme.lineHeights[0]
           : theme.lineHeights[1],
       marginBottom: leading ? theme.spacing[leading] : theme.spacing[size - 1],
-      opacity: label || T === "a" ? 0.5 : 1,
-      transition: "opacity .3s ease",
-      "&:hover": {
-        opacity: 1,
-      },
+      opacity: label ? 0.5 : 1,
     })}
     {...props}
   />
