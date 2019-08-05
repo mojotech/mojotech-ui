ca1100b450e369f72dbfaa40ae8519677e537440
chriswynn <chris@wynncodes.com>
2019-08-05 11:13:29 -0400

    Refactor TextInput component

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,70 +1,81 @@
-/** @jsx jsx */
 import * as React from "react";
-import { jsx } from "@emotion/core";
-import Text from "../Text";
+import styled from "lib/styled";
+import { spaceSet, SpaceSetProps, fontSize, FontSizeProps } from "onno-react";
+import Text from "components/Text";
+import Box from "components/Box";
 
-type FontSizes = 1 | 2 | 3 | 4 | 5;
+type TextInputProps = SpaceSetProps &
+  FontSizeProps &
+  React.HTMLProps<HTMLInputElement>;
 
-interface Props {
+interface Props extends TextInputProps {
   label?: string;
+  name?: string;
+  required?: boolean;
+  type?: string;
 }
 
-const TextInput: React.FC<Props> = ({ label = "Label", ...props }) => {
-  const [focused, setFocus] = React.useState<boolean>(false);
-  const [filled, setFilled] = React.useState<boolean>(false);
-  const [labelSize, setLabelSize] = React.useState<FontSizes>(5);
+const StyledTextInput: React.FC<Props> = styled.input<Props>(
+  spaceSet,
+  fontSize,
+  props => ({
+    border: "none",
+    borderBottom: `1px solid ${props.theme.colors.dark}30`,
+    width: "100%",
+  }),
+);
+
+export const TextInput: React.FC<Props> = props => {
+  const [focus, setFocus] = React.useState<boolean>(false);
+
+  const handleBlur = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFocus(e.target.value.length > 0);
+  };
+
+  const handleFocus = () => {
+    setFocus(true);
+  };
 
   return (
-    <div
-      css={{
-        position: "relative",
-        marginBottom:
-          "calc(24px + (56 - 24) * ((100vw - 400px) / (1800 - 600)))",
-        paddingTop: "calc(24px + (56 - 24) * ((100vw - 400px) / (1800 - 600)))",
-      }}
+    <Box
+      display="block"
+      as="label"
+      marginBottom={3}
+      position="relative"
+      height="100%"
+      width="100%"
     >
-      <label
-        css={{
-          display: "block",
-          opacity: 0.5,
-          pointerEvents: "none",
-          position: "absolute",
-          transformOrigin: "left",
-          transform:
-            focused || filled
-              ? "translateX(0px) translateY(-2.8em) scale(.9)"
-              : "translateY(0px) scale(1)",
-          transition: ".3s ease",
-        }}
+      <Text
+        bottom={0}
+        position="absolute"
+        transformOrigin="left"
+        transform={focus ? "translateY(-150%)" : "translateY(0px)"}
+        transition="transform .3s ease"
+        opacity={0}
+        as="span"
+        fontSize={1}
       >
-        <Text fontSize={labelSize}>{label}</Text>
-      </label>
-      <input
-        onBlur={e => {
-          setFocus(false);
-          setLabelSize(2);
-          e.target.value.length > 0 ? setFilled(true) : setFilled(false);
-          e.target.value.length > 0 ? setLabelSize(2) : setLabelSize(5);
-        }}
-        onFocus={e => {
-          setFocus(true);
-          setLabelSize(2);
-        }}
-        css={{
-          background: "none",
-          border: "none",
-          borderBottom: "1px solid rgba(20, 17, 29, .25)",
-          fontSize: "calc(24px + (56 - 24) * ((100vw - 400px) / (1800 - 600)))",
-          paddingBottom: ".5em",
-          width: "100%",
-          "&:focus": {
-            outline: "none",
-          },
-        }}
-        type="text"
+        {props.label}
+      </Text>
+      <StyledTextInput
+        name={props.name}
+        paddingBottom={1}
+        type={props.type}
+        onBlur={handleBlur}
+        fontSize={props.fontSize}
+        onFocus={handleFocus}
+        required={props.required}
+        aria-label={props.label}
       />
-    </div>
+    </Box>
   );
+};
+
+TextInput.defaultProps = {
+  paddingY: 1,
+  fontSize: 2,
+  label: "label",
+  type: "text",
 };
 
 export default TextInput;
--- stories/textinput.stories.tsx
+++ stories/textinput.stories.tsx
@@ -1,13 +1,14 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
 import TextInput from "../src/components/TextInput";
+import Box from "../src/components/Box";
 
 storiesOf("TextInput", module).add("Text Input", () => {
   return (
-    <div>
+    <Box paddingTop={200}>
       <TextInput label="What's your name?" />
       <TextInput label="What's your email address?" />
       <TextInput label="What problem are you trying to solve?" />
-    </div>
+    </Box>
   );
 });
