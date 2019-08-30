b31b5f1684d775df6c54acaf12f09b8cdebb3a89
chriswynn <chris@wynncodes.com>
2019-08-30 10:33:25 -0400

    Add react and react-dom to externals in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
     plugins: [new TsconfigPathsPlugin()],
     extensions: [".tsx", ".ts", ".js"],
   },
+  externals: {
+    react: "React",
+    "react-dom": "ReactDOM",
+  },
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
