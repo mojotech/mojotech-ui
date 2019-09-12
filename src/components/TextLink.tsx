38d9a765f1361e4befdc84445da9fe3806c86700
chriswynn <chris@wynncodes.com>
2019-09-12 15:34:02 -0400

    Add AnchorHTMLAttributes to TextLink

--- src/components/TextLink.tsx
+++ src/components/TextLink.tsx
@@ -2,43 +2,44 @@ import * as React from "react";
 import styled from "../lib/styled";
 import Text, { TextProps } from "./Text";
 
-export interface TextLinkProps extends TextProps {
+export type TextLinkProps = TextProps &
+  React.AnchorHTMLAttributes<HTMLAnchorElement>;
+
+interface Props extends TextLinkProps {
   scheme?: "dark" | "light";
 }
 
-const TextLink: React.FC<TextLinkProps> = styled(Text)<TextLinkProps>(
-  props => ({
-    boxShadow:
+const TextLink: React.FC<Props> = styled(Text)<Props>(props => ({
+  boxShadow:
+    props.scheme === "dark"
+      ? `0px 1px 0 ${props.theme.colors.white}30`
+      : `0px 1px 0 ${props.theme.colors.dark}30`,
+  color: "inherit",
+  cursor: "pointer",
+  textDecoration: "none",
+  position: "relative",
+  "&:after": {
+    background:
       props.scheme === "dark"
-        ? `0px 1px 0 ${props.theme.colors.white}30`
-        : `0px 1px 0 ${props.theme.colors.dark}30`,
-    color: "inherit",
-    cursor: "pointer",
-    textDecoration: "none",
-    position: "relative",
+        ? props.theme.colors.white
+        : props.theme.colors.dark,
+    bottom: -1,
+    content: "''",
+    position: "absolute",
+    height: 1,
+    left: 0,
+    transition: "transform .3s ease",
+    transform: "scaleX(0)",
+    transformOrigin: "100% 50%",
+    width: "100%",
+  },
+  "&:hover": {
     "&:after": {
-      background:
-        props.scheme === "dark"
-          ? props.theme.colors.white
-          : props.theme.colors.dark,
-      bottom: -1,
-      content: "''",
-      position: "absolute",
-      height: 1,
-      left: 0,
-      transition: "transform .3s ease",
-      transform: "scaleX(0)",
-      transformOrigin: "100% 50%",
-      width: "100%",
+      transform: "scaleX(1)",
+      transformOrigin: "0 50%",
     },
-    "&:hover": {
-      "&:after": {
-        transform: "scaleX(1)",
-        transformOrigin: "0 50%",
-      },
-    },
-  }),
-);
+  },
+}));
 
 TextLink.defaultProps = {
   as: "a",
