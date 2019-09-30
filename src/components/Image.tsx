356cfea716f5c78fe725509d585a73fda8fb0b95
chriswynn <chris@wynncodes.com>
2019-09-30 09:34:27 -0400

    Add objectFit renderer to Image component

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import styled from "../lib/styled";
 import Box, { BoxProps } from "./Box";
+import { objectFitSet, ObjectFitSetProps } from "../lib/renderers";
 
-export type ImageProps = BoxProps & React.ImgHTMLAttributes<HTMLImageElement>;
+export type ImageProps = BoxProps &
+  ObjectFitSetProps &
+  React.ImgHTMLAttributes<HTMLImageElement>;
 
-const Image: React.FC<ImageProps> = styled(Box)<ImageProps>();
+const Image: React.FC<ImageProps> = styled(Box)<ImageProps>(objectFitSet);
 
 Image.defaultProps = {
   as: "img",
