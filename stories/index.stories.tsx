2fdd0dfd80c86f0b59ba0ad02f857554ea6b2c75
chriswynn <cwynn@mojotech.com>
2021-05-04 17:15:46 -0400

    [chore] replace current ui lib with base tsdx starter

--- stories/index.stories.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { storiesOf } from "@storybook/react";
-import Button from "../src/components/Button";
-
-storiesOf("Button", module).add("with text", () => <Button>Ship it</Button>);
