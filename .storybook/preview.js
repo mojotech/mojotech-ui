e465559ab0f7823a8b06123e922d374bd171cbce
chriswynn <cwynn@mojotech.com>
2021-05-05 13:05:51 -0400

    [chore] add globalStyles to storybook preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,14 @@
+import globalStyles from '../src/lib/global-styles';
+
 // https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
 export const parameters = {
   // https://storybook.js.org/docs/react/essentials/actions#automatically-matching-args
   actions: { argTypesRegex: '^on.*' },
 };
+
+export const decorators = [
+  (Story) => {
+    globalStyles();
+    return <Story />;
+  },
+];
