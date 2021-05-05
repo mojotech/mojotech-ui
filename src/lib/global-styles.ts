f1b01d95229c2e7b9bd0e2befe7fcd1013316c74
chriswynn <cwynn@mojotech.com>
2021-05-05 12:56:41 -0400

    [feat] add css reset to global styles

--- src/lib/global-styles.ts
+++ src/lib/global-styles.ts
@@ -27,6 +27,47 @@ const globalStyles = global({
   body: {
     fontFamily: '$main',
   },
+  // reset
+  'html,body,p,ol,ul,li,dl,dt,dd,blockquote,figure,fieldset,legend,textarea,pre,iframe,hr,h1,h2,h3,h4,h5,h6': {
+    margin: 0,
+    padding: 0,
+  },
+  'h1,h2,h3,h4,h5,h6': {
+    fontSize: '100%',
+    fontWeight: 'normal',
+  },
+  ul: {
+    listStyle: 'none',
+  },
+  'button,input,select,textarea': {
+    margin: 0,
+  },
+  html: {
+    boxSizing: 'border-box',
+    '-webkit-font-smoothing': 'antialiased',
+    '-moz-osx-font-smoothing': 'grayscale',
+  },
+  '*,*:before,*:after': {
+    boxSizing: 'inherit',
+  },
+  'img,embed,iframe,object,video': {
+    height: 'auto',
+    maxWidth: '100%',
+  },
+  audio: {
+    maxWidth: '100%',
+  },
+  iframe: {
+    border: 0,
+  },
+  table: {
+    borderCollapse: 'collapse',
+    borderSpacing: 0,
+  },
+  'td,th': {
+    padding: 0,
+    textAlign: 'left',
+  },
 });
 
 export default globalStyles;
