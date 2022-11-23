cea9ae3167f0c3b47a4c4d5b23b4ec16b7e213e4
Chris Wynn <cwynn@mojotech.com>
2022-11-23 14:00:52 -0500

    chore: remove fonts

--- .storybook/theme-decorator.tsx
+++ .storybook/theme-decorator.tsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import { RenderFunction } from "@storybook/react";
 import { ThemeProvider } from "emotion-theming";
 import Reset from "../src/components/Reset";
-import Font from "../src/components/Font";
 import t from "../src/lib/theme";
 
 export default (storyFunction: RenderFunction): any => (
   <ThemeProvider theme={t}>
     <Reset />
-    <Font />
     {storyFunction()}
   </ThemeProvider>
 );
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export { zStack } from "./lib/theme";
 export { default as Button } from "./components/Button";
 export { default as Text } from "./components/Text";
 export { default as Reset } from "./components/Reset";
-export { default as Font } from "./components/Font";
 export { default as Wrap } from "./components/Wrap";
 export { default as Row } from "./components/Row";
 export { default as Column } from "./components/Column";
